8d9e9de5e7d57d82647d3502f2104ed0d8dded31
yujin <qrt365@naver.com>
2022-09-18 03:23:22 +0900

    3 commit

---
+++ BJ_9020.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_9020 {
+    public static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < a; i++) {
+            int b = Integer.parseInt(br.readLine());
+
+            prime = new boolean[b + 1];
+            get_prime();
+
+            int c = b / 2;
+            int d = b - c;
+
+            while (true) {
+                if (!prime[c] && !prime[d]) {
+                    break;
+                }
+                else {
+                    c--;
+                    d++;
+                }
+            }
+
+            System.out.println(c + " " + d);
+        }
+    }
+
+    public static void get_prime() {
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i < Math.sqrt(prime.length); i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j < prime.length; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+    }
+}
