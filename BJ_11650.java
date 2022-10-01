183027a2c874f5cd35a871c624cd40086cd1b79a
yujin <qrt365@naver.com>
2022-10-01 21:20:41 +0900

    9 commit

--- BJ_11650.java
+++ BJ_11650.java
@@ -20,11 +20,11 @@ public class BJ_11650 {
         Arrays.sort(b, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return o1[1] - o2[1]; // return 값이 양수면 위치를 바꾸지 않음 (뺄셈의 의미가 아님)
+                if (o1[0] == o2[0]) { // x 좌표가 같을 경우, y 좌표를 비교해서 정렬함
+                    return o1[1] - o2[1]; // return 값이 양수면 위치를 바꾸지 않고, 음수면 위치를 바꿈 (뺄셈의 의미가 아님)
                 }
-                else {
-                    return o1[0] - o2[0]; // return 값이 음수면 위치를 바꿈 (뺄셈의 의미가 아님)
+                else { // x 좌표가 다를 경우, x 좌표를 비교해서 정렬함
+                    return o1[0] - o2[0]; // return 값이 양수면 위치를 바꾸지 않고, 음수면 위치를 바꿈 (뺄셈의 의미가 아님)
                 }
             }
         });
@@ -57,7 +57,6 @@ public class BJ_11650 {
 //         }
 
 //         Arrays.sort(b, (o1, o2) -> (o1[0] == o2[0]) ? o1[1] - o2[1] : o1[0] - o2[0]);
-//         // 위의 람다식에서 return 값이 양수면 위치를 바꾸지 않고, 음수면 위치를 바꾼다는 의미 (뺄셈의 의미가 아님)
 
 //         for (int i = 0; i < a; i++) {
 //             sb.append(b[i][0]).append(" ").append(b[i][1]).append("\n");
