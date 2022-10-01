9a91f5ed03c0d5d44a9279c4b44d0a7fe9a058d8
yujin <qrt365@naver.com>
2022-10-01 18:06:01 +0900

    8 commit

---
+++ BJ_11650.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int[][] b = new int[a][2];
+
+        for (int i = 0; i < a; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            b[i][0] = Integer.parseInt(st.nextToken());
+            b[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(b, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for (int i = 0; i < a; i++) {
+            sb.append(b[i][0]).append(" ").append(b[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
--- BJ_1934.java
+++ BJ_1934.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BJ_1934 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
