98026f51f5c69ded0b3bad88ea33001fe4b88cc4
yujin <qrt365@naver.com>
2022-10-01 18:20:16 +0900

    9 commit

--- BJ_11650.java
+++ BJ_11650.java
@@ -1,3 +1,4 @@
+// Comparator 사용
 import java.io.*;
 import java.util.*;
 
@@ -35,3 +36,39 @@ public class BJ_11650 {
         System.out.println(sb);
     }
 }
+
+// // Lambda 사용
+// import java.io.*;
+// import java.util.*;
+
+// public class BJ_11650 {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringBuilder sb = new StringBuilder();
+
+//         int a = Integer.parseInt(br.readLine());
+//         int[][] b = new int[a][2];
+
+//         for (int i = 0; i < a; i++) {
+//             StringTokenizer st = new StringTokenizer(br.readLine());
+
+//             b[i][0] = Integer.parseInt(st.nextToken());
+//             b[i][1] = Integer.parseInt(st.nextToken());
+//         }
+
+//         Arrays.sort(b, (o1, o2) -> {
+//             if (o1[0] == o2[0]) {
+//                 return o1[1] - o2[1]; // 오름차순
+//             }
+//             else {
+//                 return o1[0] - o2[0];
+//             }
+//         });
+
+//         for (int i = 0; i < a; i++) {
+//             sb.append(b[i][0]).append(" ").append(b[i][1]).append("\n");
+//         }
+
+//         System.out.println(sb);
+//     }
+// }
