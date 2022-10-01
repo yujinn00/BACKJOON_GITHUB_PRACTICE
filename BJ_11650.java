ccb687a52ff49d495bd53187eaff60fff7af8156
yujin <qrt365@naver.com>
2022-10-01 20:30:09 +0900

    9 commit

--- BJ_11650.java
+++ BJ_11650.java
@@ -37,11 +37,11 @@ public class BJ_11650 {
     }
 }
 
-// // Lambda 사용
+// // lambda 사용
 // import java.io.*;
 // import java.util.*;
 
-// public class BJ_11650 {
+// public class Main {
 //     public static void main(String[] args) throws IOException {
 //         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //         StringBuilder sb = new StringBuilder();
@@ -56,14 +56,7 @@ public class BJ_11650 {
 //             b[i][1] = Integer.parseInt(st.nextToken());
 //         }
 
-//         Arrays.sort(b, (o1, o2) -> {
-//             if (o1[0] == o2[0]) {
-//                 return o1[1] - o2[1]; // 오름차순
-//             }
-//             else {
-//                 return o1[0] - o2[0];
-//             }
-//         });
+//         Arrays.sort(b, (o1, o2) -> (o1[0] == o2[0]) ? o1[1] - o2[1] : o1[0] - o2[0]);
 
 //         for (int i = 0; i < a; i++) {
 //             sb.append(b[i][0]).append(" ").append(b[i][1]).append("\n");
